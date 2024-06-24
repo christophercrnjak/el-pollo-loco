98e655fc2b86128bebfde9bb1b73b0e87ecf3b3c
Christopher Crnjak <christopher.crnjak@aol.de>
2024-06-24 14:15:33 +0200

    finished creating objects

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,4 +1,6 @@
 class Character extends MovableObject {
+  height = 250;
+  y = 185;
   constructor() {
     super();
     this.loadImage("../img/2_character_pepe/1_idle/idle/I-1.png");
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,4 +1,8 @@
 class Chicken extends MovableObject {
+  height = 60;
+  width = 70;
+  y = 367;
+
   constructor() {
     super();
     this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
