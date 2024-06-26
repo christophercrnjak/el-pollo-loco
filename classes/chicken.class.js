5016005d6a08636f2d1324e25ef1e009e4c5cef9
Christopher Crnjak <christopher.crnjak@aol.de>
2024-06-26 14:27:22 +0200

    normal chicken walking animation done

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,10 +2,27 @@ class Chicken extends MovableObject {
   height = 60;
   width = 70;
   y = 367;
+  IMAGES_WALKING = [
+    "../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+  currentImage = 0;
 
   constructor() {
     super();
     this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 400;
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
 }
