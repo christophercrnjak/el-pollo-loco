feae86e466e6da88b28086bef5d3d7e0379c557a
Christopher Crnjak <christopher.crnjak@aol.de>
2024-06-28 11:00:02 +0200

    no changed

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,12 +2,12 @@ class Character extends MovableObject {
   height = 250;
   y = 185;
   IMAGES_WALKING = [
-    "../img/2_character_pepe/2_walk/W-21.png",
-    "../img/2_character_pepe/2_walk/W-22.png",
-    "../img/2_character_pepe/2_walk/W-23.png",
-    "../img/2_character_pepe/2_walk/W-24.png",
-    "../img/2_character_pepe/2_walk/W-25.png",
-    "../img/2_character_pepe/2_walk/W-26.png",
+    "./img/2_character_pepe/2_walk/W-21.png",
+    "./img/2_character_pepe/2_walk/W-22.png",
+    "./img/2_character_pepe/2_walk/W-23.png",
+    "./img/2_character_pepe/2_walk/W-24.png",
+    "./img/2_character_pepe/2_walk/W-25.png",
+    "./img/2_character_pepe/2_walk/W-26.png",
   ];
   currentImage = 0;
 
