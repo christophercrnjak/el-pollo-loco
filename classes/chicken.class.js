eeb2543c6d07a81f606085668c9a12e17d7b1276
Christopher Crnjak <christopher.crnjak@aol.de>
2024-06-28 13:16:16 +0200

    no changed

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -11,7 +11,7 @@ class Chicken extends MovableObject {
 
   constructor() {
     super();
-    this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImage("../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.x = 200 + Math.random() * 400;
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
