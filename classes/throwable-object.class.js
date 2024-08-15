22d9c209d2de72d1a66f9b79e7904a3ef74e2230
Christopher Crnjak <christopher.crnjak@aol.de>
2024-08-15 16:30:18 +0200

    worked on throwing bottle

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,19 +1,23 @@
 class ThrowableObject extends MovableObject {
-  x = 200;
-  y = 350;
-  height = 90;
+  x = 100;
+  y = 100;
+  height = 70;
+  width = 60;
   speedY = 30;
   speedX = 20;
 
   constructor() {
     super();
     this.loadImage("./img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
-    this.throw(20, 30);
+    this.throw(100, 150);
   }
 
   throw(x, y) {
     this.x = x;
     this.y = y;
     this.applyGravity();
+    setInterval(() => {
+      this.x += 10;
+    }, 25);
   }
 }
