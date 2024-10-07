b0c4b9d09200c3400ae2c345497e423dc5ad3eb3
Christopher Crnjak <christopher.crnjak@aol.de>
2024-10-07 22:48:50 +0200

    tried things about gravity and falling lower - speaking about bottle

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,14 +1,13 @@
 class ThrowableObject extends MovableObject {
-  x = 100;
-  y = 100;
-  height = 70;
-  width = 60;
-  speedY = 30;
-  speedX = 20;
-
   constructor() {
     super();
     this.loadImage("./img/6_salsa_bottle/1_salsa_bottle_on_ground.png");
+    this.x = 100;
+    this.y = 100;
+    this.height = 70;
+    this.width = 60;
+    this.speedY = 30;
+    this.speedX = 20;
     this.throw(100, 150);
   }
 
