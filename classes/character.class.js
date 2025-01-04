4acb310e1b86590c82ceb5be971140f1fbffcd5f
Christopher Crnjak <christopher.crnjak@aol.de>
2025-01-04 22:42:27 +0100

    worked on animation idle and idle-long - found no fast solution of how to have slower animation for those 2 animations --> solutions i copied the images :D

--- classes/character.class.js
+++ classes/character.class.js
@@ -32,6 +32,51 @@ class Character extends MovableObject {
     "img/2_character_pepe/5_dead/D-56.png",
     "img/2_character_pepe/5_dead/D-57.png",
   ];
+  IMAGES_IDLE = [
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-1_2.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-2_2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-3_2.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-4_2.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-5_2.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-6_2.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-7_2.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-8_2.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-9_2.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png",
+    "img/2_character_pepe/1_idle/idle/I-10_2.png",
+  ];
+
+  IMAGES_IDLE_LONG = [
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-11_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19_2.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20_2.png",
+  ];
 
   world;
   walking_sound = new Audio("./audio/running.mp3");
@@ -66,6 +111,8 @@ class Character extends MovableObject {
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_HURTING);
     this.loadImages(this.IMAGES_DYING);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_IDLE_LONG);
   }
 
   animateCharacter() {
@@ -111,6 +158,8 @@ class Character extends MovableObject {
         this.playAnimation(this.IMAGES_JUMPING);
       } else if (!this.isDead() && this.isWalking()) {
         this.playAnimation(this.IMAGES_WALKING);
+      } else if (!this.isDead() && !this.isWalking() && !this.isJumping()) {
+        this.playAnimation(this.IMAGES_IDLE);
       }
     }, 1000 / 20);
   }
